7ff082bb3068a0dd1abd8cc81d3171066ce84336
kaiyin5 <louiskaiyinng@gmail.com>
2025-09-22 16:52:13 +0800

    player moving logic and update screen, grouping

--- main.py
+++ main.py
@@ -14,10 +14,16 @@ def main():
     dt = 0
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
-    # player setup
+    # grouping
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    Player.containers = (updatable, drawable)
+
+    # player instantiation
     x = SCREEN_WIDTH / 2
     y = SCREEN_HEIGHT / 2
     player = Player(x, y)
+
     while True:
         # check if the user has closed the window and exit the game loop
         # make the window's close button work.
@@ -25,7 +31,9 @@ def main():
             if event.type == pygame.QUIT:
                 return
         screen.fill("black")
-        player.draw(screen)
+        updatable.update(dt)
+        for d in drawable:
+            d.draw(screen)
         pygame.display.flip()
         dt = clock.tick(60) / 1000
 

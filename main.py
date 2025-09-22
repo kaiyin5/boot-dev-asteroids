5cec9c259660ed1b9f9fd0d35e2906dcafd95b69
kaiyin5 <louiskaiyinng@gmail.com>
2025-09-22 16:19:10 +0800

    Player class and instantiate player on screen

--- main.py
+++ main.py
@@ -1,23 +1,33 @@
 import pygame
 from constants import *
+from player import Player
 
 def main():
+    # console logging
     print("Starting Asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
+
+    # game setup
     pygame.init()
     clock = pygame.time.Clock()
     dt = 0
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
+    # player setup
+    x = SCREEN_WIDTH / 2
+    y = SCREEN_HEIGHT / 2
+    player = Player(x, y)
     while True:
         # check if the user has closed the window and exit the game loop
         # make the window's close button work.
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-        pygame.Surface.fill(screen, "black")
+        screen.fill("black")
+        player.draw(screen)
         pygame.display.flip()
-        clock.tick(60)
+        dt = clock.tick(60) / 1000
 
 
 if __name__ == "__main__":
